itislu/42-Rush-Wong_kar_Wai

commit 20d2e601c35d9560fc90fea53eda895e9ed645b7

build: Fix `make term` creating new child terminal windows

build/build.mk
@@ -7,7 +7,7 @@ include				$(MAKEFILES_DIR)/print.mk
 
 # *************************** BUILD PREPARATION ****************************** #
 
-export				CC CFLAGS MAKECMDGOALS MAKEFLAGS
+export				CC CFLAGS MAKEFLAGS
 
 
 #	Build artifacts

commit 6a47cd3e56940a7f0d93e084f16308cad012bac8

build: Fix `make term` creating new child terminal windows without side effects
Previously, commit 20d2e60 caused changes header files to not trigger a build of related sourcefiles.

The change to the dependency files inclusion is needed to still trigger a build of related source files when a header file changes.
Technically, this is only needed when `MAKECMDGOALS` would not be exported anymore, but it's still nice to have when doing `make <program_name>`.

build/build.mk
@@ -82,7 +82,8 @@ modes			:
 					fi
 					if [ "$(NEW_TERM)" = "true" ] && [ -n "$(TERMINAL)" ]; then \
 						$(TERMINAL) $(TERMINALFLAGS) bash -c \
-							"trap '' SIGINT; \
+							"unset CXX CXXFLAGS MAKECMDGOALS; \
+							trap '' SIGINT; \
 							$(ENV) ./$(NAME) $(value ARGS); \
 							exec $(shell echo $$SHELL)"; \
 					elif [ "$(RUN)" = "true" ]; then \
@@ -101,7 +102,7 @@ re				:
 #	Dependency files inclusion
 
 #	Include if building or if no target specified
-ifneq (, $(or $(filter $(BUILD_TARGETS),$(MAKECMDGOALS)),$(if $(MAKECMDGOALS),,all)))
+ifneq (, $(or $(filter $(BUILD_TARGETS) $(NAME),$(MAKECMDGOALS)),$(if $(MAKECMDGOALS),,all)))
     -include		$(DEP)
 endif
 